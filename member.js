da6232955d0ea32a49f4338dc5d75d08c467e752
M.Arshad <154744306+arshiumair@users.noreply.github.com>
2025-01-05 05:30:30 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+    return {
+        restrict: 'E',
+        templateUrl: 'app/member/member.html',
+        controller: 'SkillsMemberController',
+        controllerAs: 'vm',
+        bindToController: true,
+        scope: {
+            member: '='
+        }
+
+    };
+}
